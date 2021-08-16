fef0e6e74d5842d2b0ae808c535b06147ff61723
priscila torres reyes <priscyla.torres16@hotmail.com>
2021-08-16 17:57:22 +0000

    generator in Javascript

--- src/app.js
+++ src/app.js
@@ -7,5 +7,28 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  function textRandom() {
+    return;
+  }
+
+  let who = ["the dog", "my granma", "his turtle", "my bird"];
+  let what = ["eat", "pissed", "crushed", "broked"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  const whoPosition = Math.floor(Math.random() * (who.length - 0) + 0);
+  const WhatPosition = Math.floor(Math.random() * (what.length - 0) + 0);
+  const whenPosition = Math.floor(Math.random() * (when.length - 0) + 0);
+
+  const NewString =
+    who[whoPosition] + " " + what[whoPosition] + " " + when[whenPosition];
+
+  document.getElementById("excuse").innerHTML = NewString;
+
+  console.log(NewString);
 };
